b228bd89ffa9ca5ab49d9a1b495dcadc6c1b36e0
xvpow <c911010083@gmail.com>
2020-10-19 14:11:36 +0800

    add Sort

--- Ch11_Interface/5_Sort/main.go
+++ Ch11_Interface/5_Sort/main.go
@@ -1,26 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
+/*
+type Interface interface {
+    // Len is the number of elements in the collection.
+    Len() int
+    // Less reports whether the element with
+    // index i should sort before the element with index j.
+    Less(i, j int) bool
+    // Swap swaps the elements with indexes i and j.
+    Swap(i, j int)
+}
+*/
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("%s: %d", p.Name, p.Age)
+}
+
+type ByAge []Person
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
 func main() {
-	//可以是任意類型
-	var v1 interface{}
-	var v2 interface{}
-	v1 = 10
-	v2 = 20.56
-	fmt.Println(v1, v2)
-	//強制轉型(Type Assertion) 只能用於interface
-	m, ok := v1.(int)
-	fmt.Println(m, ok)
-	//可使用switch輸出類型
-	switch t := v2.(type) {
-	case int:
-		fmt.Println("int", t)
-	case float64:
-		fmt.Println("float", t)
-	default:
-		fmt.Println("Error")
-
+	peoples := []Person{
+		{"Ken", 45},
+		{"Lucy", 31},
+		{"Iris", 19},
+		{"Vivin", 26},
 	}
+	fmt.Println(peoples)
+	//把people 轉型為 ByAge
+	//https://golang.org/pkg/sort/#Sort
+	sort.Sort(ByAge(peoples))
+	fmt.Println(peoples)
 
+	//SliceStable Sort
+	//https://golang.org/pkg/sort/#example_SliceStable
+	// sort.SliceStable(peoples, func(i, k int) bool {
+	// 	return peoples[i].Age < peoples[k].Age
+	// })
+	// fmt.Println(peoples)
 }
