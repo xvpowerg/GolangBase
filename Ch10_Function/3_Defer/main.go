6ee828e4dcf3b003504bef5d2a8168bef120a852
xvpow <c911010083@gmail.com>
2020-10-20 09:29:43 +0800

    add Defer Vargs

--- Ch10_Function/3_Defer/main.go
+++ Ch10_Function/3_Defer/main.go
@@ -6,7 +6,6 @@ func testDefer() {
 	defer fmt.Println("testDefer")
 	fmt.Println("Test2")
 	fmt.Println("Test3")
-
 }
 
 func testDeferOrder() {
@@ -17,7 +16,15 @@ func testDeferOrder() {
 	defer fmt.Println("Test1_3")
 }
 
+func testVar(i int) {
+	//i變數會被儲存
+	defer fmt.Println("TestVar Setp1:", i)
+	i++
+	defer fmt.Println("TestVar Setp2:", i)
+}
+
 func main() {
 	testDefer()
 	//testDeferOrder()
+	//testVar(1)
 }
