cad2f62df11bbaabdd68b5b962b7c3144df09328
xvpow <c911010083@gmail.com>
2020-10-23 09:59:53 +0800

    add readme

--- Ch5_Operators/2_comp_operator/main.go
+++ Ch5_Operators/2_comp_operator/main.go
@@ -5,10 +5,16 @@ import "fmt"
 func main() {
 	v1 := 15
 	v2 := 2
+	//大於
 	fmt.Printf("%v > %v =%t\n", v1, v2, v1 > v2)
+	//小於
 	fmt.Printf("%d < %d =%v\n", v1, v2, v1 < v2)
+	//大於等於
 	fmt.Printf("%d >= %d =%v\n", v1, v2, v1 >= v2)
+	//小於等於
 	fmt.Printf("%d <= %d =%v\n", v1, v2, v1 <= v2)
+	//等於
 	fmt.Printf("%d == %d =%v\n", v1, v2, v1 == v2)
+	//不等於
 	fmt.Printf("%d != %d =%v\n", v1, v2, v1 != v2)
 }
--- Ch5_Operators/3_logical_op/main.go
+++ Ch5_Operators/3_logical_op/main.go
@@ -5,7 +5,10 @@ import "fmt"
 func main() {
 	b1 := true
 	b2 := false
+	//且
 	fmt.Printf("%t && %t = %t\n", b1, b2, b1 && b2)
+	//或
 	fmt.Printf("%t || %t = %t\n", b1, b2, b1 || b2)
+	//反向
 	fmt.Printf("!%t  = %t\n", b1, !b1)
 }
