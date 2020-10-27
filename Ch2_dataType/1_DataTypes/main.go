007867d363e4db27621b2c34a5a6e367527b1983
xvpow <c911010083@gmail.com>
2020-10-27 12:50:45 +0800

    add readme

--- Ch2_dataType/1_DataTypes/main.go
+++ Ch2_dataType/1_DataTypes/main.go
@@ -2,28 +2,49 @@ package main
 
 import "fmt"
 
-//宣告在方法外時必須使用var
-var p1 = 83
+//宣告在函數外時必須使用var
+var p1 = 85
 var p2 int = 72
+var p3 int32 = 95
 
 func main() {
-	//宣告在方法內可使用:=
-	x := 42 + 7
-	y := "Howard"
-	fmt.Println(x)
-	fmt.Println(y)
-	var z = 71
-	fmt.Println(z)
-	//給定類型
-	var age byte = 100
-	fmt.Println(age)
-	//複數
-	var point complex64 = 10 + 20i
-	fmt.Println(point)
-	//一次宣告多個變數
-	x1, y2 := 10, 20
-	fmt.Println(x1, y2)
-	//數字交換
-	x1, y2 = y2, x1
-	fmt.Println(x1, y2)
+
+	fmt.Println(p1, p2, p3)
+	//宣告在函數內可使用:=
+	// x := 56
+	// y := "Howard"
+	// fmt.Println(x)
+	// fmt.Println(y)
+	// var z = 71
+	// fmt.Println(z)
+
+	// //給定類型
+	// var age byte = 100
+	// fmt.Println("age:", age)
+	// var height float32 = 178.56
+	// fmt.Println("height:", height)
+	// var weight int32 = 75
+	// fmt.Println("weight:", weight)
+
+	// //複數
+	// var point complex64 = 10.2 + 20.56i
+	// fmt.Println(point)
+
+	// //一次宣告多個變數
+	// x1, y2 := 10, 20
+	// fmt.Println(x1, y2)
+	// //數字交換
+	// x1, y2 = y2, x1
+	// fmt.Println(x1, y2)
+
+	// var a1, b2 int32 = 75, 63
+	// fmt.Println(a1, b2)
+
+	//bool
+	var isPass bool = true
+	fmt.Println(isPass)
+	isOpen := false
+	fmt.Println(isOpen)
+	isOpen = true
+	fmt.Println(isOpen)
 }
--- Ch2_dataType/3_CustomType/main.go
+++ Ch2_dataType/3_CustomType/main.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	type DataInfo int
+	//type 關鍵字表示宣告某種類型
+	//DataInfo 自定義的類型名稱
+	//int32 表示DataInfo 可存放的內容
+	type DataInfo int32
 	var data1 DataInfo = 10
 	//%d 表示整數 %T 表示類型
 	fmt.Printf("%d %T", data1, data1)
+	type Student string
 
 }
