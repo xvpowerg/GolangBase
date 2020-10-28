dfab29a50513eba74fe96d455ddb5232f3374783
xvpow <c911010083@gmail.com>
2020-10-28 09:46:30 +0800

    add readme

--- Ch2_dataType/4_defaultvalue/main.go
+++ Ch2_dataType/4_defaultvalue/main.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var height int
+	var height int32
 	fmt.Println(height)
+	var weight float64
+	fmt.Println(weight)
+	//fmt.Printf("%.1f\n", weight)
+	var isOpen bool
+	fmt.Println(isOpen)
+	var name string
+	fmt.Println(name)
 	//slices
 	var values []int
 	fmt.Println(values == nil)
+	fmt.Println("values:", values)
 	var myFun func()
 	fmt.Println("myFun:", myFun)
 
