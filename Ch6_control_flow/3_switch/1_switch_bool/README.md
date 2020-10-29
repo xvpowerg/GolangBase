fb6f094bb18f96721fca56095ca52a00e8bdde19
xvpow <c911010083@gmail.com>
2020-10-29 12:17:59 +0800

    chnage switch

--- Ch6_control_flow/3_switch/1_switch_bool/README.md
+++ Ch6_control_flow/3_switch/1_switch_bool/README.md
@@ -1,1 +1,2 @@
-# Switch
+# Switch 
+基本用法
--- Ch6_control_flow/3_switch/1_switch_bool/main.go
+++ Ch6_control_flow/3_switch/1_switch_bool/main.go
@@ -4,6 +4,27 @@ import "fmt"
 
 func main() {
 
+	name := "Ken"
+	switch name {
+	case "Vivin":
+		fmt.Println("業務")
+	case "Ken":
+		fmt.Println("經理")
+	case "Lucy":
+		fmt.Println("PM")
+	}
+
+	switch {
+	case name == "Vivin":
+		fmt.Println("業務")
+	case name == "Ken":
+		fmt.Println("經理")
+	case name == "Lucy":
+		fmt.Println("PM")
+
+	}
+
+	//可使用常數
 	type Action int
 	const (
 		PLAY Action = iota
