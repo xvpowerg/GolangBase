b2db2d3e9a53f34295b906dabf4275419ad88e86
xvpow <c911010083@gmail.com>
2020-11-04 21:45:01 +0800

    change io

--- Ch12_IO/1_Read/2_TestFileExist/README.md
+++ Ch12_IO/1_Read/2_TestFileExist/README.md
@@ -1,16 +1,14 @@
 # [TestFileExist](https://golang.org/pkg/os/#File.Stat)
 查看檔案是否存在透過判斷Error
 ```go
-f, _ := os.Open(`C:\MyDir\msg.txt`)
-_, err := f.Stat()
-if err == nil {
-	fmt.Println("exist!")
-} 
-//也可用封裝好的 os.Stat與 os.IsNotExist 搭配
-filePath := `C:\MyDir\msg.txt`
-_, err2 := os.Stat(filePath)
-fmt.Println(os.IsNotExist(err2))
 
+filePath := `C:\MyDir\msg.txt`
+	_, err2 := os.Stat(filePath)
+	if os.IsNotExist(err2) {
+		fmt.Println("檔案不存在!")
+	} else {
+		fmt.Println("檔案存在!")
+	}
 ```
 
 
--- Ch12_IO/1_Read/2_TestFileExist/main.go
+++ Ch12_IO/1_Read/2_TestFileExist/main.go
@@ -7,17 +7,12 @@ import (
 
 func main() {
 
-	f, _ := os.Open(`C:\MyDir\msg.txt`)
-	_, err := f.Stat()
-	if err == nil {
-		fmt.Println("exist!")
-	} else if os.IsNotExist(err) {
-		fmt.Println("IsNotExist!")
-	}
-	//也可用封裝好的 os.Stat與 os.IsNotExist 搭配
+	// os.Stat與 os.IsNotExist 搭配查看檔案是否存在
 	filePath := `C:\MyDir\msg.txt`
 	_, err2 := os.Stat(filePath)
-	fmt.Println(os.IsNotExist(err2))
-	//os.IsExist 是給檔案存在 但沒權限讀取時使用
-
+	if os.IsNotExist(err2) {
+		fmt.Println("檔案不存在!")
+	} else {
+		fmt.Println("檔案存在!")
+	}
 }
