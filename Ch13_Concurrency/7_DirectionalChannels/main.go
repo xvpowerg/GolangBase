f6bca3c9b8252c7b9eea045a7b43ae04d5794861
xvpow <c911010083@gmail.com>
2020-11-13 21:25:45 +0800

    change channel code

--- Ch13_Concurrency/7_DirectionalChannels/main.go
+++ Ch13_Concurrency/7_DirectionalChannels/main.go
@@ -2,22 +2,14 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
-func init() {
-	rand.Seed(time.Now().UnixNano()) //因為要使用亂數必須給予亂數的來源
-}
 func testRecive(ch <-chan int) {
 	fmt.Printf("v :%d", <-ch)
 }
 
 func testSend(ch chan<- int) {
-	fmt.Println("資料計算中...")
-	time.Sleep(time.Duration(2) * time.Second)
-	fmt.Println("資料完成傳送資料...")
-	ch <- rand.Int()
+	ch <- 10
 }
 
 func main() {
