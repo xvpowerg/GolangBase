3fab2ebfb60819f907fe6ea68e99e4bd83eb2275
xvpow <c911010083@gmail.com>
2020-11-13 21:42:44 +0800

    change channel code

--- Ch13_Concurrency/HomeWork/main.go
+++ Ch13_Concurrency/HomeWork/main.go
@@ -1,43 +1,5 @@
 package main
 
-import "fmt"
-
-func receive1(evenChan, oddChan <-chan int, quit <-chan bool) ([]int, []int) {
-	evenList := make([]int, 0)
-	oddList := make([]int, 0)
-	for {
-		select {
-		case even := <-evenChan:
-			evenList = append(evenList, even)
-		case odd := <-oddChan:
-			oddList = append(oddList, odd)
-		case <-quit:
-			return evenList, oddList
-		}
-
-	}
-
-}
-
-func send1(even, odd chan<- int, quit chan<- bool) {
-	for i := 1; i <= 100; i++ {
-		switch {
-		case i%2 == 0:
-			even <- i
-		default:
-			odd <- i
-		}
-	}
-	close(quit)
-}
-
 func main() {
-
-	evenChan := make(chan int)
-	oddChan := make(chan int)
-	quit := make(chan bool)
-	go send1(evenChan, oddChan, quit)
-	evenList, oddList := receive1(evenChan, oddChan, quit)
-	fmt.Println(evenList, oddList)
 
 }
--- Ch13_Concurrency/README.md
+++ Ch13_Concurrency/README.md
@@ -1,11 +1,2 @@
 # Concurrency
-## 作業
-* 幫我用evenChan,oddChan兩個Channel做 1~100 當中偶數與奇數，並使用evenList與oddList收集偶數奇數
-```go
-evenChan := make(chan int)
-oddChan := make(chan int)
-evenList, oddList := receive1(....)
-......
-```
-
 
